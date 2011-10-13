0afd9f03c11c18239770b239db0c3fb64d31cdd1
Milan Patel <patelmilan.p@gmail.com>
2011-10-13 20:42:57 +0530

    Done with sign in

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -36,4 +36,28 @@ describe "Users" do
       end
     end
   end
+
+  describe "sign in/out" do
+
+    describe "failure" do
+
+      it "should not sign a user in" do
+        visit signin_path
+        fill_in :email,    :with => ""
+        fill_in :password, :with => ""
+        click_button
+        response.should have_selector("div.flash.error", :content => "Invalid")
+      end
+    end
+
+    describe "success" do
+
+      it "should sign a user in and out" do
+        integration_sign_in(Factory(:user))
+        controller.should be_signed_in
+        click_link "Sign out"
+        controller.should_not be_signed_in
+      end
+    end
+  end
 end
