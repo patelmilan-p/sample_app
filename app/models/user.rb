fe1bb8f08a3a4e9c475665286c44507096dccaff
Milan Patel <patelmilan.p@gmail.com>
2011-12-02 10:52:55 +0530

    Added user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,9 +10,12 @@
 #
 
 require 'digest'
+
 class User < ActiveRecord::Base
   attr_accessor :password
   attr_accessible :name, :email, :password, :password_confirmation
+
+  has_many :microposts, :dependent => :destroy
 
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
@@ -42,6 +45,10 @@ class User < ActiveRecord::Base
   def self.authenticate_with_salt(id, cookie_salt)
     user = find_by_id(id)
     (user && user.salt == cookie_salt) ? user : nil
+  end
+
+  def feed
+    Micropost.where("user_id = ?", id)
   end
 
   private
