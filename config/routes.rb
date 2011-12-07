6e891202d3f32fc80228a49079f6997afebdc43d
Milan Patel <patelmilan.p@gmail.com>
2011-12-07 11:41:40 +0530

    Added user following

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,13 @@ SampleApp::Application.routes.draw do
 
   resources :users do
     resources :microposts, :only => :index
+    member do
+      get :following, :followers
+    end
   end
-  resources :sessions,   :only => [:new, :create, :destroy]
-  resources :microposts, :only => [:create, :destroy]
+  resources :sessions,      :only => [:new, :create, :destroy]
+  resources :microposts,    :only => [:create, :destroy]
+  resources :relationships, :only => [:create, :destroy]
 
   match '/signup',  :to => 'users#new'
   match '/signin',  :to => 'sessions#new'
